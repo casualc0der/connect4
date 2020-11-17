d457dab8e024191ef8947f2dfbfac807bbed62b6
casualc0der <edwardsansome@gmail.com>
2020-11-17 22:20:30 +0000

    Add space below accessor

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,6 @@
 class Player
   attr_reader :name, :disc
+
   def initialize(name, disc)
     raise 'Name should be 50 characters or less' if name.length > 50
     raise 'Use single character for disc' if disc.length > 1
